1c7de6b4b906885030bd804cc0b54e5001c21f19
Naomi Hisano <eduardanaomihisano@gmail.com>
2025-04-07 18:38:29 -0300

    Mobile
    07/04/2025

---
+++ Mobile/projetoMobile/App.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <AntDesign name="hearto" size={24} color="black" />
+
+    <img>
+
+</img>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
