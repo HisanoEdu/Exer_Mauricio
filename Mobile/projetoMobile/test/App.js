4978ad05f4ab04f231dc00fea9839e6005cde3a6
Naomi Hisano <eduardanaomihisano@gmail.com>
2025-04-08 18:38:11 -0300

    Mobile
    Mobile

--- Mobile/projetoMobile/test/App.js
+++ Mobile/projetoMobile/test/App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AntDesign name="hearto" size={24} color="black" />
+
+    <img>
+
+</img>
     </View>
+
   );
 }
 
